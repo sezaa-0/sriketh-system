8ffa1518d5bd15cd263fc54d265b3c37bcb00411
Sadeepa Sewmina <sadeepasew2005@gmail.com>
2026-06-08 08:22:05 +0530

    Fix layout bug by removing duplicate sidebar wrapper from fuel page

--- app/(dashboard)/fuel/layout.js
+++ app/(dashboard)/fuel/layout.js
@@ -4,9 +4,5 @@ export const metadata = {
 };
 
 export default function FuelLayout({ children }) {
-  return (
-    <div className="relative left-1/2 w-screen max-w-[100vw] -translate-x-1/2 overflow-x-clip bg-white">
-      {children}
-    </div>
-  );
+  return children;
 }
